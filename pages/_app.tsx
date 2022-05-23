32bbc4a2f0e989aab90c0d551c8e29ece52abf65
Martin Jordán <martin.jordan@reservio.com>
2022-05-23 21:14:49 +0200

    New: Use ChakraProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, theme, CSSReset } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
